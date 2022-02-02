3550ac0bce905c3c424c26b8affaf0330e410546
HoomanIrani <78396095+HoomanIrani@users.noreply.github.com>
2022-02-01 22:16:35 -0800

    Add files via upload

---
+++ ASCII_ART/src/E11_1.java
@@ -0,0 +1,21 @@
+public class E11_1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("\n._=(  Pac-Man [7] )=_.");
+        System.out.println("==================================================.");
+        System.out.println("     .-.    .-.      .--.                         |");
+        System.out.println("    | OO|  | OO|    / _.-' .-.   .-.  .-.  .''.   |");
+        System.out.println("    |   |  |   |    \\  '-. '_'   '_'  '_'  '..'   |");
+        System.out.println("    '^^^'  '^^^'     '__'                         |");
+        System.out.println("                                                  |");
+        System.out.println("================.  .-.  .=================.  .-.  |");
+        System.out.println("                | |   | |                 |  '-'  |");
+        System.out.println("                | |   | |                 |       |");
+        System.out.println("                | ':-:' |                 |  .-.  |");
+        System.out.println("142             |  '_'  |                 |  '-'  |");
+        System.out.println("================'       '================='       |");
+
+
+    }
+}
